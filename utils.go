d8f02aa91eba4fb08528b87476f27fba15569649
mishamolnar <michaellmuller423@gmail.com>
2023-10-22 14:46:22 +0300

    new non backward compatible version

--- utils.go
+++ utils.go
@@ -2,6 +2,6 @@ package golang_dependency_two
 
 import "strings"
 
-func CapitalizeString(str string, another string) string {
-	return strings.ToUpper(str) + another
+func CapitalizeString(str string, another string, delimiter string) string {
+	return strings.ToUpper(str) + delimiter + another
 }
